91ef75ef71183ac97dc1f52b49cff7e6207fbb53
Joe Fong <joe.fong@madetech.com>
2022-03-08 14:06:48 +0000

    refactor chance method

--- ruby/yatzy.rb
+++ ruby/yatzy.rb
@@ -9,13 +9,9 @@ class Yatzy
   end
 
   def self.chance(d1, d2, d3, d4, d5)
-    total = 0
-    total += d1
-    total += d2
-    total += d3
-    total += d4
-    total += d5
-    return total
+    
+    [d1, d2, d3, d4, d5].sum
+
   end
 
   def self.yatzy(dice)
