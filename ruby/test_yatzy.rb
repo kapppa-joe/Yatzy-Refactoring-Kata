b3c87ae62513f88b998014da72f6de11a5e3d6c9
Joe Fong <joe.fong@madetech.com>
2022-03-08 14:16:10 +0000

    refactor yatzy tests

--- ruby/test_yatzy.rb
+++ ruby/test_yatzy.rb
@@ -8,9 +8,7 @@ class YatzyTest < Test::Unit::TestCase
   end
 
   def test_yatzy_scores_50
-    expected = 50
-    actual = Yatzy.yatzy([4,4,4,4,4])
-    assert expected == actual
+    assert 50 == Yatzy.yatzy([4,4,4,4,4])
     assert 50 == Yatzy.yatzy([6,6,6,6,6])
     assert 0 == Yatzy.yatzy([6,6,6,6,3])
   end
